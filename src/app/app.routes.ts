7157851cecfe129f3920b2dd2b025b5d1c698824
rnoboa <betodeveloper90@hotmail.com>
2018-09-18 14:52:44 -0500

    Creating modules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 
-
-import { PagesComponent } from './pages/pages.component';
-import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { Graphs1Component } from './pages/graphs1/graphs1.component';
 import { RegisterComponent } from './login/register.component';
-
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 const appRoutes: Routes = [
-    { 
-        path: '',
-        component: PagesComponent,
-        children: [
-            { path: 'dashboard', component: DashboardComponent},
-            { path: 'progress', component: ProgressComponent},
-            { path: 'graphs1', component: Graphs1Component},
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-        ]
-    },
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
     { path: '**', component: NopagefoundComponent}
