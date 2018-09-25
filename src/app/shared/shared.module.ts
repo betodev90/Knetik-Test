5a1d9ef5136d01c41c87de8e36cc24f535b855b8
rnoboa <betodeveloper90@hotmail.com>
2018-09-25 17:28:10 -0500

    Link

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NopagefoundComponent } from '../nopagefound/nopagefound.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
+    imports: [
+        RouterModule,
+        CommonModule,
+    ],
     declarations: [
         NopagefoundComponent,
         HeaderComponent,
